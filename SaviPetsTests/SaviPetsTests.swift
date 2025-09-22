0d1c8d0f27d3eedd0fc6fd7dd8727accb7988afd
Savi <dk0133@proton.com>
2025-09-21 21:53:17 -0400

    Initial Commit

---
+++ SaviPetsTests/SaviPetsTests.swift
@@ -0,0 +1,17 @@
+//
+//  SaviPetsTests.swift
+//  SaviPetsTests
+//
+//  Created by K!MO on 9/21/25.
+//
+
+import Testing
+@testable import SaviPets
+
+struct SaviPetsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
